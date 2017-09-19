a99470b0fb877abdc96b44febe95cd74a38498eb
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-09-18 22:16:49 -0300

    funciones para identificar datos de mp3

---
+++ src/id.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "id3v2lib/include/id3v2lib.h"
+
+char * get_artist(char * nombre_archivo) {
+	FILE *archivo;
+	archivo = fopen(nombre_archivo, "r");
+	ID3v2_tag * tag = load_tag(nombre_archivo);
+	ID3v2_frame * artista = tag_get_artist(tag);
+	ID3v2_frame_text_content * contenido_artista = parse_text_frame_content(artista);
+	return contenido_artista->data;
+}
+
+char * get_genre(char * nombre_archivo) {
+	FILE *archivo;
+	archivo = fopen(nombre_archivo, "r");
+	ID3v2_tag * tag = load_tag(nombre_archivo);
+	ID3v2_frame * genero = tag_get_genre(tag);
+	ID3v2_frame_text_content * contenido_genero = parse_text_frame_content(genero);
+	return contenido_genero->data;
+}
+
+void get_all(char * nombre_archivo, char * art, char * gnr) {
+	FILE *archivo;
+	archivo = fopen(nombre_archivo, "r");
+	ID3v2_tag * tag = load_tag(nombre_archivo);
+	ID3v2_frame * artista = tag_get_artist(tag);
+	ID3v2_frame * genero = tag_get_genre(tag);
+	ID3v2_frame_text_content * contenido_artista = parse_text_frame_content(artista);
+	ID3v2_frame_text_content * contenido_genero = parse_text_frame_content(genero);
+	art = contenido_artista->data;
+	gnr = contenido_genero->data;
+}
