8e1085c6e7d7f23d0be526ed59cfcc9178d89ca1
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-09-18 22:19:27 -0300

    cerrado archivo

--- src/id.c
+++ src/id.c
@@ -7,6 +7,7 @@ char * get_artist(char * nombre_archivo) {
 	ID3v2_tag * tag = load_tag(nombre_archivo);
 	ID3v2_frame * artista = tag_get_artist(tag);
 	ID3v2_frame_text_content * contenido_artista = parse_text_frame_content(artista);
+	fclose(archivo);
 	return contenido_artista->data;
 }
 
@@ -16,6 +17,7 @@ char * get_genre(char * nombre_archivo) {
 	ID3v2_tag * tag = load_tag(nombre_archivo);
 	ID3v2_frame * genero = tag_get_genre(tag);
 	ID3v2_frame_text_content * contenido_genero = parse_text_frame_content(genero);
+	fclose(archivo);
 	return contenido_genero->data;
 }
 
@@ -29,4 +31,5 @@ void get_all(char * nombre_archivo, char * art, char * gnr) {
 	ID3v2_frame_text_content * contenido_genero = parse_text_frame_content(genero);
 	art = contenido_artista->data;
 	gnr = contenido_genero->data;
+	fclose(archivo);
 }
