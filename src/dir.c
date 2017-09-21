c304bc5d9a6ff80c7514c7c55c004b7d1c5ad669
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-09-21 00:27:29 -0300

    arreglado segmentation fault, avance insercion

--- src/dir.c
+++ src/dir.c
@@ -6,6 +6,9 @@
 #include "id3v2lib/include/id3v2lib.h"
 #include "../include/header.h"
 
+//#include "id.c"				// /* Borrar para el
+//#include "estructuras.c"	//		Makefile	*/
+
 int getExt(char * archivo, char * extension) {
 	char * ext = strrchr(archivo, '.');
 	if (!ext) {
@@ -17,9 +20,12 @@ int getExt(char * archivo, char * extension) {
 	}
 }
 
-void listSongs(lista * canciones) {
+int listSongs(lista * canciones) {
 	struct dirent *entrada;
-	DIR * carpeta = opendir("../Biblioteca De Musica");
+	DIR * carpeta = opendir("Biblioteca De Musica");
+	if (carpeta == NULL) {
+        return 0;
+    }
     while ((entrada = readdir(carpeta)) != NULL) {
     	char * extension = (char *)malloc(sizeof(char));
     	if (getExt(entrada->d_name, extension)) {
@@ -29,6 +35,33 @@ void listSongs(lista * canciones) {
     	};
     }
     closedir(carpeta);
+    return 1;
+}
+
+void insertSong(lista * generos, lista * canciones) {
+	nodo * actual = canciones->inicial;
+	unsigned int i = 0;
+	char * biblioteca = "Biblioteca De Musica/";
+	printf("fuera insertSong de largo: %d\n", canciones->largo); // test
+	while (i < canciones->largo) {
+		char * cancion = actual->contenido;
+		printf("contenido %s\n", cancion);
+		char * path = (char *)malloc(strlen(cancion)+strlen(biblioteca)+1);
+		strcpy(path, biblioteca);
+		printf("path 1: %s\n", path);
+		strcat(path, cancion);
+		printf("path 2: %s\n", path);
+		char * gen = get_genre(path);
+		printf("Saque el genero %s\n", gen);
+		char * art = get_artist(path);
+		printf("Saque el artista %s\n", art);
+		insertarGen(generos, gen);
+		insertarArt(generos, gen, art);
+		i++;
+		if (i < canciones->largo) {
+			actual = actual->sig;
+		}
+	}
 }
 
 void readFolders() {
@@ -43,9 +76,10 @@ void readSong() {
 
 }
 
-int main() {
-	char * extension = (char *)malloc(sizeof(char));
-	int asd = getExt("hola.qwerty", extension);
-	printf("Encontre la wea?: %d\n", asd);
-	printf("Y la ext era: %s\n", extension);
-}
+/*int main() {
+	lista * canciones = (lista *)malloc(sizeof(lista));
+	listSongs(canciones);
+	lista * genre = (lista *)malloc(sizeof(lista));
+	insertSong(genre, canciones);
+	free(canciones);
+}*/
