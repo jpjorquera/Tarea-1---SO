1aa9a5c612a6c9edbf8d248e2f8e04620d41a94f
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-09-21 00:26:33 -0300

    actualizadas funciones

--- include/header.h
+++ include/header.h
@@ -2,9 +2,16 @@
 #define HEADER_H_
 
 #include "estructuras.h"
-
+// id
 char * get_artist(char * archivo);
 char * get_genre(char * archivo);
 void get_all(char * archivo, char * artista, char * genero);
+// dir
+int getExt(char * archivo, char * extension);
+int listSongs(lista * canciones);
+void insertSong(lista * generos, lista * canciones);
+void readFolders();
+void enterFolder();
+void readSong();
 
 #endif
