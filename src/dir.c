d789b247bd0c243b778af5fd2fa62445302bccc3
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-09-28 01:11:42 -0300

    arreglado manejo biblioteca

--- src/dir.c
+++ src/dir.c
@@ -18,8 +18,8 @@ int getExt(char * archivo, char ** extension) {
 
 int enlistSongs(lista * canciones) {
 	struct dirent * entrada;
-	DIR * carpeta = opendir("Biblioteca De Musica");
-	char * biblioteca = "./Biblioteca De Musica/";
+	DIR * carpeta = opendir("biblioteca");
+	char * biblioteca = "./biblioteca/";
 	if (carpeta == NULL) {
         return 0;
     }
@@ -36,6 +36,7 @@ int enlistSongs(lista * canciones) {
 				strncat(path, entrada->d_name, strlen(entrada->d_name));
 				// Abriendo archivo
     			FILE * archivo = fopen(path, "rb");
+                //printf("%s\n", path);
     			// Obtener tag
     			get_all(archivo, tag);
     			// Verificard version ID3v1
@@ -55,28 +56,28 @@ int enlistSongs(lista * canciones) {
 }
 
 void createFolders(lista * canciones) {
-    char * biblioteca = "Biblioteca de Musica/";
+    char * biblioteca = "./biblioteca/";
     struct stat st = {0};
     int i, j;
     nodo * auxi = canciones->inicial;
     nodo * auxj;
     i = 0;
     while (i < canciones->largo) {
-        int largo_str_gen = strlen(biblioteca) + strlen(auxi->contenido);
-        char * path_gen = (char *)malloc(sizeof(char) * largo_str_gen);
+        int largo_str_gen = strlen(biblioteca) + strlen(auxi->contenido) + 1;
+        char * path_gen = (char *)calloc(largo_str_gen, sizeof(char));
         strcpy(path_gen, biblioteca);
-        strcat(path_gen, auxi->contenido);
+        strncat(path_gen, auxi->contenido, strlen(auxi->contenido));
         if (stat(path_gen, &st) == -1) {
                 mkdir(path_gen, 0700);
             }
         auxj = auxi->subcontenido->inicial;
         j = 0;
         while (j < auxi->subcontenido->largo) {
-            int largo_str_art = largo_str_gen + strlen(auxj->contenido) + 1;
-            char * path_art = (char *)malloc(sizeof(char) * largo_str_art);
+            int largo_str_art = largo_str_gen + strlen(auxj->contenido) + 2;
+            char * path_art = (char *)calloc(largo_str_art, sizeof(char));
             strcpy(path_art, path_gen);
-            strcat(path_art, "/");
-            strcat(path_art, auxj->contenido);
+            strncat(path_art, "/", 1);
+            strncat(path_art, auxj->contenido, strlen(auxj->contenido));
             if (stat(path_art, &st) == -1) {
                 mkdir(path_art, 0700);
             }
@@ -95,30 +96,32 @@ void createFolders(lista * canciones) {
 }
 
 void moveSongs(lista * canciones) {
-    char * biblioteca = "Biblioteca de Musica/";
+    char * biblioteca = "./biblioteca/";
     struct dirent * entrada;
-    DIR * carpeta = opendir("Biblioteca De Musica");
+    DIR * carpeta = opendir("biblioteca");
+    int largo;
     while ((entrada = readdir(carpeta)) != NULL) {
         char * extension = (char *)calloc(5, sizeof(char));
         if (getExt(entrada->d_name, &extension)) {
             if (!strncmp(extension, "mp3", 3)) {
-                mp3tag * tag_actual = (mp3tag *)malloc(sizeof(mp3tag));
-                char * path = (char *)malloc(sizeof(char)*(strlen(entrada->d_name)+strlen(biblioteca)+1));
+                mp3tag * tag_actual = (mp3tag *)calloc(1, sizeof(mp3tag));
+                largo = strlen(entrada->d_name) + strlen(biblioteca) + 1;
+                char * path = (char *)calloc(largo, sizeof(char));
                 strcpy(path, biblioteca);
-                strcat(path, entrada->d_name);
-                FILE * archivo = fopen(path, "r");
+                strncat(path, entrada->d_name, strlen(entrada->d_name));
+                FILE * archivo = fopen(path, "rb");
                 // Obtener tag
                 get_all(archivo, tag_actual);
                 if (verifyTag(tag_actual->tag)) {
                     int largo_dir = strlen(biblioteca) + strlen(tag_actual->genre) 
-                                    + strlen(tag_actual->artist) + strlen(entrada->d_name)+ 2;
-                    char * new_path = (char *)malloc(sizeof(char) * largo_dir);
+                                    + strlen(tag_actual->artist) + strlen(entrada->d_name) + 5;
+                    char * new_path = (char *)calloc(largo_dir, sizeof(char));
                     strcpy(new_path, biblioteca);
-                    strcat(new_path, tag_actual->genre);
-                    strcat(new_path, "/");
-                    strcat(new_path, tag_actual->artist);
-                    strcat(new_path, "/");
-                    strcat(new_path, entrada->d_name);
+                    strncat(new_path, tag_actual->genre, strlen(tag_actual->genre));
+                    strncat(new_path, "/", 1);
+                    strncat(new_path, tag_actual->artist, strlen(tag_actual->artist));
+                    strncat(new_path, "/", 1);
+                    strncat(new_path, entrada->d_name, strlen(entrada->d_name));
                     rename(path, new_path);
                     free(new_path);
                 }
